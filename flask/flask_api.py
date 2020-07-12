ca98daa90feeb650ef5eb54520a143f7d9954939
วุฒิชัย แก้วล้อมทรัพย์ <wuttichaihung@gmail.com>
2020-07-12 19:37:45 +0700

    animation plot

---
+++ flask/flask_api.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, jsonify
+app = Flask(__name__)
+
+@app.route("/", methods=["POST", "GET"])
+def hello():
+    data_dict = {"a":3, "b":True}
+    return jsonify(data_dict)
+
+@app.route("/home/<name>")
+def home(name):
+    return render_template("home.html",title="ABC", name=name)
+
+
+if __name__ == "__main__":
+  app.run(host="localhost", port=5000, debug=True)
