0d3bec8f90a3e5a3040ec224ba7dae9ad82b96eb
wuttichai-hung <61757850+wuttichai-hung@users.noreply.github.com>
2020-08-18 16:38:09 +0700

    Create send_email.py

---
+++ send_email.py
@@ -0,0 +1,15 @@
+import os
+import smtplib
+
+EMAIL_ADDRESS = "mail"
+EMAIL_PASSWORD = "pass"
+
+EMAIL_RECIEVER = "mail"
+subject = "Test send email"
+body = "how are you"
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.starttls()
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    msg = f'Subject:{subject}\n\n{body}'
+    smtp.sendmail(EMAIL_ADDRESS, "RECIEVER", msg)
